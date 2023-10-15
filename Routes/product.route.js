e0f45da3e144518f45f3b0d9f7a441031621fa43
Reynaldy Marchell <reynalldymarchell03@gmail.com>
2023-10-15 23:11:44 +0700

    Product routing

---
+++ Routes/product.route.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req,res) => {
+    res.send('getting a list of all products');
+});
+
+router.post('/', (req,res) => {
+    res.send('products crated');
+});
+
+router.get('/:id', (req,res) => {
+    res.send('getting a single product');
+});
+
+router.patch('/:id', (req,res) => {
+    res.send('updating a single product');
+});
+
+router.delete('/:id', (req,res) => {
+    res.send('deleting a single product');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const port = 3000
+const port = 3000;
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+const ProductRoute = require('./Routes/product.route');
+app.use('/products',ProductRoute);
+
+
+
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
